1c93214744d7da82bb3169204dac998605ac6d4c
findmyburger <122633337+findmyburger@users.noreply.github.com>
2023-01-13 18:18:37 +0100

    Add files via upload

---
+++ FIndMyBurger/FIndMyBurger/FIndMyBurgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIndMyBurgerApp.swift
+//  FIndMyBurger
+//
+//  Created by Apps2T on 13/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FIndMyBurgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
