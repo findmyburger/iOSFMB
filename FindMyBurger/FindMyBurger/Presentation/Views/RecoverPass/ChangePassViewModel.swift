7e8d115a7e682836447073bb09516aa1c3b40696
JuanJose <jhonajose@hotmail.es>
2023-02-14 17:48:55 +0100

    FMB-148/Conexión con Backend Recuperar Contraseña
    Se ha creado la conexión con la API par mandar el email y cambiar la contraseña.

--- FindMyBurger/FindMyBurger/Presentation/Views/RecoverPass/ChangePassViewModel.swift
+++ FindMyBurger/FindMyBurger/Presentation/Views/RecoverPass/ChangePassViewModel.swift
@@ -13,7 +13,7 @@ class ChangePassViewModel: ObservableObject{
     @Published var shouldShowError: Bool = false
     
     let userDefaults = UserDefaults.standard
-
+    
     @Published var alertText: String = ""
     
     func changePass(pass: String, pass2: String, code: String){
@@ -32,15 +32,47 @@ class ChangePassViewModel: ObservableObject{
                     alertText = "El código no es correcto."
                 }else{
                     let url = "http://127.0.0.1:8000/api/users/updateData"
-
+                    
                     let dictionary: [String: Any] = [
                         "password": pass
                     ]
-                    //connectToAPI(dictionary: dictionary, url: url)
+                    connectToAPI(dictionary: dictionary, url: url)
                 }
-               
+                
             }
             
         }
     }
+    func connectToAPI(dictionary: [String: Any], url: String){
+        NetworkHelper.shared.requestProvider(url: url, params: dictionary) { data, response, error in
+            if let error = error {
+                self.onError(error: error.localizedDescription)
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 {
+                    self.onSuccess(data)
+                } else {
+                    self.onError(error: error?.localizedDescription ?? "Request Error")
+                }
+            }
+        }
+    }
+    func onSuccess(_ data: Data) {
+        do {
+            let updatePassResponse = try JSONDecoder().decode(UpdateDataResponseModel?.self, from: data)
+            
+            if updatePassResponse?.status == 200 {
+                shouldShowLogin = true
+            }else{
+                shouldShowError = true
+                alertText = updatePassResponse?.message ?? "Algo ha salido mal al cambiar la contraseña."
+            }
+        } catch {
+            self.onError(error: error.localizedDescription)
+        }
+    }
+    func onError(error: String) {
+        shouldShowError = true
+        alertText = error
+    }
+    
 }
--- FindMyBurger/FindMyBurger/Presentation/Views/RecoverPass/SendEmailViewModel.swift
+++ FindMyBurger/FindMyBurger/Presentation/Views/RecoverPass/SendEmailViewModel.swift
@@ -13,7 +13,7 @@ class SendEmailViewModel: ObservableObject{
     @Published var shouldShowError: Bool = false
     
     let userDefaults = UserDefaults.standard
-
+    
     @Published var alertText: String = ""
     
     func sendEmail(email: String){
@@ -22,12 +22,47 @@ class SendEmailViewModel: ObservableObject{
             alertText = "Rellena todos los campos."
         }else{
             let url = "http://127.0.0.1:8000/api/users/sendEmail"
-
+            
             let dictionary: [String: Any] = [
                 "email": email
             ]
             //connectToAPI(dictionary: dictionary, url: url)
         }
     }
+    func connectToAPI(dictionary: [String: Any], url: String){
+        NetworkHelper.shared.requestProvider(url: url, params: dictionary) { data, response, error in
+            if let error = error {
+                self.onError(error: error.localizedDescription)
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                if response.statusCode == 200 {
+                    self.onSuccess(data)
+                } else {
+                    self.onError(error: error?.localizedDescription ?? "Request Error")
+                }
+            }
+        }
+    }
+    func onSuccess(_ data: Data) {
+        do {
+            let sendEmailResponse = try JSONDecoder().decode(SendEmailResponseModel?.self, from: data)
+            
+            if sendEmailResponse?.status == 200 {
+                shouldShowChangePass = true
+                
+                userDefaults.set(sendEmailResponse?.data?.code, forKey: "code")
+                
+            }else{
+                shouldShowError = true
+                alertText = sendEmailResponse?.message ?? "Algo ha salido mal al enviar el email."
+            }
+        } catch {
+            self.onError(error: error.localizedDescription)
+        }
+    }
+    func onError(error: String) {
+        shouldShowError = true
+        alertText = error
+    }
+    
 }
 
