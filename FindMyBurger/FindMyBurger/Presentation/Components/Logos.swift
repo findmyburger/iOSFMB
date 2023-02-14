fbde7817d76eddb2e2a7090b32887ad8953997eb
JuanJose <jhonajose@hotmail.es>
2023-02-14 16:57:25 +0100

    FMB-144/Aplicar Diseño Recuperar Contraseña.
    Se ha empezado el diseño de recuperar contraseña.

--- FindMyBurger/FindMyBurger/Presentation/Components/Logos.swift
+++ FindMyBurger/FindMyBurger/Presentation/Components/Logos.swift
@@ -9,13 +9,19 @@ import Foundation
 import SwiftUI
 
 struct Logo2: View {
-    
     var body: some View{
-        
         Image("Logo2")
             .resizable()
             .frame(width: 115, height: 90)
             .padding(.bottom, 5)
     }
-   
 }
+struct Logo3: View {
+    var body: some View{
+        Image("Logo3")
+            .resizable()
+            .frame(width: 148, height: 163)
+            .padding(.bottom, 5)
+    }
+}
+
