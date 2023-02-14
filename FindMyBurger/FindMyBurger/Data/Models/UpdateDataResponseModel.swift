03c94d60a5296fdd4dccad41afed794909bec07b
JuanJose <jhonajose@hotmail.es>
2023-02-14 17:44:29 +0100

    FMB-147/Lógica Recuperar Contraseña
    Se ha creado la lógica de recuperar la contraseña.

---
+++ FindMyBurger/FindMyBurger/Data/Models/UpdateDataResponseModel.swift
@@ -0,0 +1,14 @@
+//
+//  UpdateDataResponseModel.swift
+//  FindMyBurger
+//
+//  Created by Juan jose Morales on 14/2/23.
+//
+
+import Foundation
+
+struct UpdateDataResponseModel: Decodable{
+    let status: Int?
+    let message: String?
+    //let data: DataToUpdateResponseModel?
+}
