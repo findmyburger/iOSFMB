d3caf3732511d9afa4d3bf04acca9526023bb383
mario <mariohgcte@gmail.com>
2023-02-17 18:45:58 +0100

    Diseño de perfil
    He aplicado el diseño del perfil y editar perfil.

---
+++ FindMyBurger/FindMyBurger/Presentation/Views/Profile/ViewModels/EditProfileViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  EditProfileViewModel.swift
+//  FindMyBurger
+//
+//  Created by Apps2T on 17/2/23.
+//
+
+import SwiftUI
+
+struct EditProfileViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileViewModel()
+    }
+}
---
+++ FindMyBurger/FindMyBurger/Presentation/Views/Profile/ViewModels/ProfileViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewModel.swift
+//  FindMyBurger
+//
+//  Created by Apps2T on 17/2/23.
+//
+
+import SwiftUI
+
+struct ProfileViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileViewModel()
+    }
+}
